5c90658c28ecccc4698ecf0b6a8b69bf17faad07
charlottejanssens <r0783142@student.vives.be>
2021-02-05 13:06:31 +0100

    commit switch 5/05/2021

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ function groepIndeling(datum){
 
     var indeling = datum %7 + 1;
     console.log("je zit in de groep: " + indeling);
+    switch(indeling){
+        case 1 : console.log("aaaaa")
+        break;
+        case 2 : console.log("bbbb")
+        break;
+        case 3 : console.log("cccc")
+        break;
+        case 4 : console.log("dddd")
+        break;
+        case 5 : console.log("eeee")
+        break;
+        case 6 : console.log("fffff")
+        break;
+        case 7 : console.log("ggggg")
+        break;
+    }
 }
 
 const readline = require("readline");
@@ -12,5 +28,7 @@ const rl = readline.createInterface({
 
 rl.question("toets uw geboortedatum in (YYYYMMDD): ", function (datum) {
     groepIndeling(datum);
+    
     rl.close();
 });
+
